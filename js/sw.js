4bfcd4d638bb3e962b96da67d99be2e7903d2a58
sukma hadi <jfshark008@outlook.com>
2026-07-01 12:31:35 +0700

    deploy

--- js/sw.js
+++ js/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'reko2stamp.kolase-v3';
+const CACHE_NAME = 'reko2stamp.kolase-v4';
 const APP_ROOT = new URL(self.registration.scope || './', self.location.href);
 
 const ASSETS_TO_CACHE = [
